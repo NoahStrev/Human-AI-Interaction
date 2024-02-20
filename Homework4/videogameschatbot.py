0b4b3aa5e730a3d4c53e1402650f5a0e2c1dcb82
NoahStrev <121129688+NoahStrev@users.noreply.github.com>
2024-02-20 13:09:12 -0600

    Creating a basic video game chatbot

---
+++ Homework4/videogameschatbot.py
@@ -0,0 +1,43 @@
+import ast
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+questions = []
+answers = []
+
+with open('qa_Video_Games.json', 'r') as f:
+    for line in f:
+        data = ast.literal_eval(line)
+        questions.append(data['question'].lower())
+        answers.append(data['answer'].lower())
+
+# Use CountVectorizer to convert the questions list into a sparse matrix
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+vectorizer = CountVectorizer(stop_words='english')
+X_vec = vectorizer.fit_transform(questions)
+tfidf = TfidfTransformer(norm='l2')
+X_tfidf = tfidf.fit_transform(X_vec)
+
+
+def conversation(quest):
+    global tfidf, answers, X_tfidf
+    # quest is the user's question that we (the chatbot) is to answer
+    Y_vec = vectorizer.transform(quest)
+    Y_tfidf = tfidf.fit_transform(Y_vec)
+
+    angle = np.rad2deg(np.arccos(max(cosine_similarity(Y_tfidf, X_tfidf)[0])))
+    if angle > 60:
+        return "Sorry, I did not quite understand that"
+    else:
+        return answers[np.argmax(cosine_similarity(Y_tfidf, X_tfidf)[0])]
+
+user = input('Please enter your name: ')
+print('Hi', user, " I am a rep of Q&A support. How can I help you?")
+while True:
+    userquestion = input("{}: ".format(user))
+    if userquestion.lower() == 'bye':
+        print('Q&A support: bye! Have a good day!')
+        break
+    else:
+        print('The question is: ', userquestion)
+        print('Q&A support: ', conversation([userquestion]))
